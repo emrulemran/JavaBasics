94dd34468ec8017528786b6f5aab8676619d39b2
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-06-25 00:04:19 -0400

    Create DecimalToBinary.java

---
+++ src/conversions/DecimalToBinary.java
@@ -0,0 +1,8 @@
+package conversionz;
+
+public class DecimalToBinary {
+	public static void main(String[] args) {
+		Integer i = new Integer(12);
+		System.out.println(Integer.toBinaryString(i));
+	}
+}
