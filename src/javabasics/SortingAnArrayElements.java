9c55b680fb67f45823ceefbeec306e01e93c265a
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-08-01 23:36:02 -0400

    Create SortingAnArrayElements.java

---
+++ src/javabasics/SortingAnArrayElements.java
@@ -0,0 +1,17 @@
+package classthree;
+
+import java.util.Arrays;
+
+public class SortingAnArrayElements {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 10, 2, 3, 1, 59, 61, 17, 8, 91, 0 };
+
+		Arrays.sort(nums);
+
+		System.out.println(Arrays.toString(nums));
+    
+	}
+
+}
