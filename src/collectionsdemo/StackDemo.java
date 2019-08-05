c61e86b32ccae9b62eee4548ac6cedf0503a8994
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-08-05 00:38:51 -0400

    Create StackDemo.java

---
+++ src/collectionsdemo/StackDemo.java
@@ -0,0 +1,25 @@
+package collectionsdemo;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String args[]) {
+		Stack<String> stk = new Stack<String>();
+		stk.add("Ostrich");
+		stk.add("Iguana");
+		stk.add("Boa");
+		stk.add("Crocodile");
+		stk.add("Zebra");
+		stk.add("Jackle");
+		stk.add("Anaconda");
+		stk.add("Tapir");
+		System.out.println(stk);
+		stk.pop();
+		Iterator<String> itr = stk.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+}
