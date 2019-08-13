7e2b6e179d945fb8bfb89768778075b4fa41a635
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-08-12 23:49:52 -0400

    Rename DecimalToBinary.java to Decimal2Binary.java

---
+++ src/conversions/Decimal2Binary.java
@@ -0,0 +1,16 @@
+package stringdemo;
+
+public class DecimalToBinary {
+
+	public static void main(String a[]) {
+
+		int decimal = 124;
+		System.out.println("Decimal: " + decimal);
+		System.out.println("Binary representation of " + decimal + ": " + Integer.toBinaryString(124));
+
+	}
+}
+
+Output:
+Binary: 124
+Binary representation of 124: 1111100
