1db1544ed56124b9e295527b346f96b8ade150e9
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-09-13 00:05:01 -0500

    Create EscapeSequenceDemo.java

---
+++ src/javabasics/EscapeSequenceDemo.java
@@ -0,0 +1,19 @@
+package exceldemo;
+
+public class EscapeSequenceDemo {
+
+	public static void main(String[] args) {
+		// double-quote example:
+		System.out.println("the quote is: \"man is mortal\"");
+
+		// single-quote example:
+		System.out.println("This is New York's best street food!");
+
+		// tab example:
+		System.out.println("\tIts a tab example!");
+
+		// new line example:
+		System.out.println("One line and \nA new line!");
+	}
+
+}
