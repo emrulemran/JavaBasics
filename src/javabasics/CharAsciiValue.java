b571b7132b3af52fc7aa9b4c6fd2cd5f49773fa6
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-09-14 22:52:08 -0500

    Create CharAsciiValue.java

---
+++ src/javabasics/CharAsciiValue.java
@@ -0,0 +1,12 @@
+package arraydemo;
+
+public class CharAsciiValue {
+	public static void main(String[] args) {
+		char ch = 'a';
+		int castAscii = (int) ch;
+		System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+	}
+}
+
+
+Output: The ASCII value of a is: 97
