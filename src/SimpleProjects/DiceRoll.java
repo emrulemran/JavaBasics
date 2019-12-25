69581bf1f0e303489ea816565ab66f28aac9750d
MD EMRUL EMRAN <emrulhasan@gmail.com>
2019-12-25 13:36:51 -0500

    Update DiceRoll.java

--- src/SimpleProjects/DiceRoll.java
+++ src/SimpleProjects/DiceRoll.java
@@ -7,7 +7,7 @@ public class DiceRoll {
 	public static void main(String[] args) {
 
 		Random rand = new Random();
-		int randNum = rand.nextInt(6);
+		int randNum = rand.nextInt(6) + 1;   // to exclude 0
 
 		System.out.println("The random number seen on the top of the dice is: " + randNum);
 
