d6b571f8d2aefb2268d7662b9a3db503cdb7ffb7
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-03-25 22:11:46 -0400

    Create ExceptionsExamples.java

---
+++ src/ExceptionPractice/ExceptionsExamples.java
@@ -0,0 +1,62 @@
+package jdbcpak;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import javax.imageio.stream.FileImageInputStream;
+
+public class ExceptionsExamples {
+
+	public static void main(String[] args) {
+		// #1 Arithmetic exception:
+		int x = 19;
+		int y = 0;
+		int division = x / y;
+		System.out.println(division);
+
+		// # 2 StringIndexOutOfBoundsException:
+		String city = "newyorkcity";
+		System.out.println(city.substring(1, 15));
+		System.out.println(city.charAt(13));
+
+		// #3 ArrayIndexOutOfBoundsException:
+		String[] names = { "Joha", "Raman", "Aziz", "Anne", "Polash" };
+		System.out.println(names[5]);
+
+		// #4 NullPointerException:
+		String anything = null; 
+		System.out.println(anything.length());
+
+		// #5 NumberFormatException:
+		String number = "12dfd34";
+		int convertedNumber = Integer.parseInt(number);
+		System.out.println(convertedNumber);
+
+		// #6 and 7 ClassNotFoundException and SQLException:
+		Class.forName("com.mysql.cj.jdbc.Driver");					// ClassNotFoundException 
+		Connection conn = DriverManager.getConnection("abasfdfd", "root", "4444");  // SQLException
+		Statement st = conn.createStatement();
+		ResultSet rset = st.executeQuery("we are happy learners");
+
+		// #8 InterruptedException:
+		Thread.sleep(5000);
+
+		// 9 and 10 FileNotFoundException and IOException:
+		File file = new File("C:\\Users\\aaaa\\dddd\\demo.txt");  	// FileNotFoundException - invalid file path
+		FileImageInputStream fis = new FileImageInputStream(file);	// IOException
+		System.out.println(fis);
+
+		// #11 IllegalFormatConversionException:
+		int DegreesF;
+		double DegreesC;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter a whole number as Farenheit: ");
+		DegreesF = keyboard.nextInt();
+		DegreesC = (5 * (DegreesF - 32)) / 9;
+		System.out.printf(DegreesF + " degree Farenheit is equal to " + "%2.2f", DegreesC + " degree Celcius");
+	}
+}
