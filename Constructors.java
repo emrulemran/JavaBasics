37dd07010121da95bad1c5eac9be13be364926c8
MD EMRUL EMRAN <emrulhasan@gmail.com>
2025-11-21 00:02:58 -0500

    Create Constructors.java
    no final keyword

---
+++ Constructors.java
@@ -0,0 +1,34 @@
+package testcodes;
+
+public class Constructors {
+	
+	// final keyword is not permitted
+	// final is an Illegal modifier for the constructor in type Constructors; only public, protected & private are permitted
+
+	// parameterless constructor:
+	 Constructors() {
+		// default or zero-implementation constructor
+	}
+
+	// constructor overloading with parameterized constructors:
+	Constructors(int a) {
+
+	}
+
+	Constructors(String name) {
+
+	}
+
+	Constructors(int id, String fullName) {
+
+	}
+
+	Constructors(int accountNo, String name, float balance) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
